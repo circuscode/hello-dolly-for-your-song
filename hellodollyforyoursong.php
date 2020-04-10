05953f652083c94bdcceb1382e9dca9cfebfd960
circuscode <marco.hitschler@spelzenhof.com>
2020-04-10 17:03:44 +0200

    Bugfix Plugin Administration Link

--- hellodollyforyoursong.php
+++ hellodollyforyoursong.php
@@ -49,9 +49,9 @@ Customizing @ Plugin Page
 
 /* This adds the settings link on the plugin page */
 function hdfys_add_plugin_page_links ( $links ) {
-	$hdfys_links = array('<a href="' . admin_url( 'options-general.php?page=hellodollyforyoursong' ) . '">'. __('Options','hello-dolly-for-your-song').'</a>',);
+	$hdfys_links = array('<a href="' . admin_url( 'options-general.php?page=hdfys_settings.php' ) . '">'. __('Options','hello-dolly-for-your-song').'</a>',);
 	return array_merge( $links, $hdfys_links );
-	}
-add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'hdfys_add_plugin_page_links' );
+}
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'hdfys_add_plugin_page_links' );
 
 ?>

46c7c81b78129045873ab4653130800c8e0dfea6
circuscode <marco.hitschler@spelzenhof.com>
2020-05-09 15:34:37 +0200

    Load Text Domain earlier

--- hdfys_translations.php
+++ hdfys_translations.php
@@ -22,6 +22,6 @@ if (!defined('ABSPATH'))
 function hdfys_load_textdomain() {
 	load_plugin_textdomain('hello-dolly-for-your-song', false, dirname( plugin_basename( __FILE__ ) ) . '/languages');
 }
-add_action( 'init', 'hdfys_load_textdomain' );
+add_action( 'plugins_loaded', 'hdfys_load_textdomain' );
 
 ?>
